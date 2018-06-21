979afaa7676678e94860e97bd76f488e8ccd52fb
chenxianxi <576729125@qq.com>
2018-06-21 11:28:07 +0800

    x

--- src/main/java/com/liuaoz/model/demo/Demo.java
+++ src/main/java/com/liuaoz/model/demo/Demo.java
@@ -7,8 +7,6 @@ import lombok.*;
  * Created by matrix_stone on 2018/6/20.
  */
 @Data
-@ToString(callSuper = true)
-@EqualsAndHashCode(callSuper = false)
 public class Demo extends BaseEntity{
 
     private String name;
